68d4486346a9f28ff85c5de4a4383d55588da853
AnthLima <tryout3699@gmail.com>
2022-10-01 23:09:18 -0300

    ensure LoginRouter returns 400 if no password is provider

--- src/src/presentation/routers/login-router/login-router.spec.js
+++ src/src/presentation/routers/login-router/login-router.spec.js
@@ -1,18 +1,30 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
     }
   }
 }
+
 describe('Login Router', () => {
   test('Should return 400 if email not found', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if password not found', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'anyemail@email.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
