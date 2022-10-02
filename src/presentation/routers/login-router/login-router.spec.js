8647b2f13b7ad0bdfaaeffc7e599c5493751a4b2
AnthLima <tryout3699@gmail.com>
2022-10-02 17:19:35 -0300

    move LoginRouter to its own file

---
+++ src/presentation/routers/login-router/login-router.spec.js
@@ -0,0 +1,39 @@
+const LoginRouter = require('./login-router')
+const MissingParamError = require('../../helpers/missing-param-error')
+describe('Login Router', () => {
+  test('Should return 400 if email not found', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if password not found', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Should return 500 if httpRequest not found', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
+  })
+})
